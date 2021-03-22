fc0ef42f7809102ae33c600a633831340d281120
champab <akchamp3366@gmail.com>
2021-03-22 10:16:56 +0530

    local change:day2

--- main1.go
+++ main1.go
@@ -1,7 +1,10 @@
-package main 
-import "fmt"
-func main() {
- fmt.Printf("hello world bye")
- fmt.Printf("Second code")
+package main
 
- }
+import "fmt"
+
+func main() {
+	fmt.Printf("hello world bye")
+	fmt.Printf("Second code")
+	fmt.Printf("Second code2")
+	fmt.Printf("Third code: Day2")
+}
